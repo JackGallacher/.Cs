d75545113fbde55182eecf46c21996b4d84078ea
Jack Gallacher <jackleslie.gallacher@gmail.com>
2015-09-24 11:33:37 +0100

    Workshop 1
    First workshop of the Level 1 Programming module.

---
+++ Workshop1/Workshop1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop1
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+              
+            Console.Write("Hello \n\t World!\n\t\tI am learning \n\t\t\t   C# \nIt is easy!\n\n");
+            Console.WriteLine("Unit: CMP1127M \"Programming and Data Structures\"");
+            Console.WriteLine("Todays date it: " + DateTime.Now);
+
+            Console.ReadLine();
+        }
+    }
+}
