22d9fd540fc5ee4e3e1a08bcd502b5dac264eb81
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-02-02 16:20:17 +0000

    Added Week 12 Workshop
    Added Week 12 Workshop
    - Creating a Queue using arrays.

---
+++ C# Demonstrating/12 - Using Arrays to Create Queues/12 - Using Arrays to Create Queues/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Workshop12
+{
+    public class MyQueue
+    {
+        //the queue is a first in first out data structure. e.g. the element in the position [0] will always be the first to be removed, as it was the first element entered.
+        private int back = 0;
+        private int[] my_queue;
+
+        public MyQueue(int x)//override construcor requiring size specification to init queue.
+        {
+            Console.WriteLine("Creating a Queue with the size of {0}.\n", x);
+            my_queue = new int[x];
+        }
+
+        public void add(int x)//add an item onto the back of the queue.
+        {
+            try
+            {
+                Console.WriteLine("Adding {0} to the back of the queue.", x);
+                my_queue[back] = x;
+                back++;//increments the queue count when an element is added.
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("The array is full, please remove the first element to add somthing else to the back of the queue");
+                Console.Read();//waits for any key to be pressed.
+                Environment.Exit(0);//then closes the application.
+            }
+        }
+        public void remove()//remove an item from the front of the queue.
+        {
+            Console.WriteLine("Removing the element from the front of the queue, that number is {0}.\n", my_queue[0]);
+            my_queue[0] = 0;
+            shuffle(my_queue);
+            back--;
+        }
+        public void shuffle(int[] array)//shuffle items in the queue (to move them down one place).
+        {
+            Console.WriteLine("Shuffling the queue, moving each item down one place in the queue\n");
+            for (int i = 0; i < back; i++)//as the count is now 1 less due to it being "--" in the remove function, when myqueue[i] = my_queue[i + 1] hapenns it will be like myqueue[0] = my_queue[1], shfting the values up one space.
+            {
+                my_queue[i] = my_queue[i + 1];
+            }
+        }
+        public void display()
+        {
+            Console.WriteLine("Displaying Queue\n");
+            for(int i = 0; i < my_queue.Length; i++)
+            {
+                Console.WriteLine("Queue position [{0}] = {1}\n", i, my_queue[i]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue queue = new MyQueue(5);
+
+            queue.add(5);
+            queue.add(6);
+            queue.add(7);
+            queue.display();
+
+            queue.add(8);
+            queue.display();
+
+            queue.remove();
+            queue.display();
+
+            queue.remove();
+            queue.display();
+
+            Console.Read();
+        }
+    }
+}
---
+++ C# Demonstrating/12 - Using Arrays to Create Queues/12 - Using Arrays to Create Queues/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("12 - Using Arrays to Create Queues")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("12 - Using Arrays to Create Queues")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("03b14b02-6579-41c5-8b92-dc5b839399f7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
